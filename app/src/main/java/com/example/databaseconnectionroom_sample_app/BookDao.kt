36bb9ed6002809298b217da9153492445d709cdd
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-03-04 23:55:39 +0900

    second commit

---
+++ app/src/main/java/com/example/databaseconnectionroom_sample_app/BookDao.kt
@@ -0,0 +1,29 @@
+package com.example.databaseconnectionroom_sample_app
+
+import androidx.room.*
+
+@Dao
+interface BookDao{
+
+
+    //crud operations -->Create(Insert),read(select),update,delete
+    //@Query("INSERT INTO books_table(book_name,author_name)")
+    //values("abc","pasan")WHERE id =10
+
+    @Insert()
+    fun insertBook(book: Book)
+
+    @Query("SELECT * FROM books_table")
+    fun getAllBooks(): List<Book>
+
+    //SET book_name = "DEF".author_name = "sam"
+    //WHERE id = 2
+    @Update()
+    fun updateBook(book: Book)
+
+    //DELETE from books_table
+    //WHERE id = 8
+    @Delete()
+    fun deleteBook(book: Book)
+
+}
